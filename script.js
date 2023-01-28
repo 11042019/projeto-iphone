c90eb6d30363036af486fd45699e5c7c9e06d12b
Eduardo Siqueira <111020001+11042019@users.noreply.github.com>
2023-01-28 16:59:53 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const botao = document.getElementById('botao')
-var horas = document.getElementById('horas')
-
-function clicar(evento){
-    data = new Date()
-    console.log(data)
-    horas.innerHTML = data
-}
-botao.addEventListener('click', clicar)
